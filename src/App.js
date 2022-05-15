36c464628f0668a78882f83d5e678e74c343b290
justwaitme <contact.hamnar@gmail.com>
2022-05-15 15:16:25 +0200

    dark mode include

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import './index.css'
+import NavBar from './components/NavBar';
+import Main from './components/Main';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode()
+  {
+    setDarkMode(prevDarkMode => !prevDarkMode)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Main darkMode={darkMode}/>
     </div>
   );
 }
